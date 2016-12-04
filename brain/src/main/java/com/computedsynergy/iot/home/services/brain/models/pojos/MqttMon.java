fa19f22d331c9b3fa877b0186a28da26b852384f
Faisal Thaheem <faisal.ajmal@gmail.com>
2016-12-04 11:03:56 +0500

    Adding missing files

---
+++ brain/src/main/java/com/computedsynergy/iot/home/services/brain/models/pojos/MqttMon.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.computedsynergy.iot.home.services.brain.models.pojos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Faisal Thaheem
+ */
+
+public class MqttMon {
+    
+    private int id;
+    private String topicname;
+    private String payload;
+    private Date created;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the topicname
+     */
+    public String getTopicname() {
+        return topicname;
+    }
+
+    /**
+     * @param topicname the topicname to set
+     */
+    public void setTopicname(String topicname) {
+        this.topicname = topicname;
+    }
+
+    /**
+     * @return the payload
+     */
+    public String getPayload() {
+        return payload;
+    }
+
+    /**
+     * @param payload the payload to set
+     */
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+
+    /**
+     * @return the created
+     */
+    public Date getCreated() {
+        return created;
+    }
+
+    /**
+     * @param created the created to set
+     */
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+    
+    
+}
